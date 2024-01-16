40c9f7618a32d1f295720c1aa0b69918ce438ac5
Vonique Stricklen <vstrickl.git@gmail.com>
2024-01-15 20:55:15 -0800

    Created PDF Export format

--- resume/urls.py
+++ resume/urls.py
@@ -4,5 +4,7 @@ from . import views
 
 urlpatterns = [
     path("", views.resume, name="resume"),
+    path("preview", views.resume_export, name="resume_export"),
+    path("generate_pdf", views.generate_pdf, name="generate_pdf"),
     path("upload_skills", views.upload_skills, name="upload_skills"),
 ]
